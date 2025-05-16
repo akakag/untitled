b85ee1ba8e4abff67c5e19ed9f5a249c49c93c2a
241081 <241081@std.hi-joho.ac.jp>
2025-05-16 12:25:02 +0900

    kari

---
+++ src/kko.java
@@ -0,0 +1,7 @@
+public class kko {
+public static void main(String[] args) {
+    //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
+    // IntelliJ IDEA によるその修正案を確認できます。
+    System.out.printf("k");
+}
+}
